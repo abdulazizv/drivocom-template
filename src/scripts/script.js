92466c4327a07342fee123f9dfa73d7b287d4551
abdulazizv <abdulazizheir@gmail.com>
2023-02-15 18:55:37 +0500

    header almost endded

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,2 +1,78 @@
 "use strict";
 
+const mode = document.querySelector(".mode");
+const modeIcon = document.querySelector("#mode-icon");
+const loginBtn = document.querySelector("#btn-login");
+const login = document.querySelector(".login");
+const form_exit = document.querySelector(".form_exit");
+const iconEye = document.querySelector(".icon-eye");
+const password = document.querySelector("#password");
+const email = document.querySelector("#email");
+const form = document.querySelector(".form");
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+  localStorage.theme = "dark";
+  modeIcon.removeAttribute("src");
+  modeIcon.setAttribute("src", "./images/Iconsun.svg");
+} else {
+  document.documentElement.classList.remove("dark");
+  localStorage.theme = "light";
+  modeIcon.removeAttribute("src");
+  modeIcon.setAttribute("src", "./images/Icon.svg");
+}
+
+//  Whenever the user explicitly chooses light mode
+
+//  Whenever the user explicitly chooses dark mode
+mode.addEventListener("click", (e) => {
+  if (localStorage.theme === "dark") {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+    modeIcon.removeAttribute("src");
+    modeIcon.setAttribute("src", "./images/Icon.svg");
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+    modeIcon.removeAttribute("src");
+    modeIcon.setAttribute("src", "./images/Iconsun.svg");
+  }
+});
+
+// !----login modal
+loginBtn.addEventListener("click", (e) => {
+  login.classList.remove("hidden");
+  login.classList.add("flex");
+});
+
+login.addEventListener("click", (e) => {
+  if (e.target.classList.contains("login")) {
+    login.classList.add("hidden");
+    login.classList.remove("flex");
+    password.value = "";
+    email.value = "";
+  }
+});
+
+form_exit.addEventListener("click", (e) => {
+  login.classList.add("hidden");
+  login.classList.remove("flex");
+  password.value = "";
+  email.value = "";
+});
+
+iconEye.addEventListener("click", (e) => {
+  if (e.target.classList.contains("bxs-show")) {
+    iconEye.classList.remove("bxs-show");
+    iconEye.classList.add("bxs-hide");
+    password.type = "text";
+  } else {
+    iconEye.classList.remove("bxs-hide");
+    iconEye.classList.add("bxs-show");
+    password.type = "password";
+  }
+});
