f6122b3a16d9acb76b2897094658bf6eb539339e
abdulazizv <abdulazizheir@gmail.com>
2023-02-16 12:53:28 +0500

    opinion section added

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -44,6 +44,7 @@ mode.addEventListener("click", (e) => {
 // !----login modal
 loginBtn.addEventListener("click", (e) => {
   login.classList.remove("hidden");
+  login.classList.add("z-20");
   login.classList.add("flex");
 });
 
@@ -442,3 +443,114 @@ vanscarbutton.addEventListener('click',() => {
     card_wrapper.append(element);
   });
 })
+
+
+
+const comments = [
+  {
+    id: 1,
+    avatar: "./img/savannah nguyen.png",
+    name: "Savannah Nguyen",
+    email: "savannahnguyen@gmail.com",
+    comment: `Sagittis sed tortor, elementum vitae varius sodales
+    consectetur. Vitae arcu ut morbi bibendum ullamcorper
+    feugiat. Venenatis hendrerit cursus egestas sit ornare nunc
+    vulputate aliquam sapien. Pulvinar nunc consectetur nibh
+    consectetur mollis mauris est mauris amet. Sed hac vehicula
+    id et dui morbi quam pulvinar condimentum. Senectus sed
+    varius interdum nunc, arcu vulputate massa.`,
+    rating: "./img/Frame 5292.svg",
+  },
+  {
+    id: 2,
+    avatar: "./img/ronald richards.png",
+    name: "Ronald Richards",
+    email: "ronaldrichards@gmail.com",
+    comment: `Placerat pulvinar in amet sem id gravida nisi leo. A dignissim est pellentesque mi quisque vel mi ut. Porttitor blandit auctor odio a ornare felis tortor bibendum molestie. `,
+    rating: "./img/Frame 5292 (1).svg",
+  },
+  {
+    id: 3,
+    avatar: "./img/kristan watson.png",
+    name: "Kristin Watson",
+    email: "kristinwatson@gmail.com",
+    comment: `Id adipiscing vulputate arcu pretium dignissim. Felis vulputate tellus velit consectetur. Congue convallis aliquam eget ullamcorper quam pellentesque cursus. Dictumst mauris ac, quis orci morbi ultrices faucibus volutpat mauris. Bibendum sed ut lectus facilisi amet orci. Interdum massa orci urna id ac cras arcu.`,
+    rating: "./img/Frame 5292.svg",
+  },
+];
+const comment_list = document.querySelector(".comment_list");
+const scrollbar = document.querySelector(".scrollbar");
+comments.forEach((el) => {
+  const element = createElement(
+    "li",
+    "comment__item w-[420px] h-[333px] rounded-2xl bg-white dark:bg-[#1C1C1C] p-6",
+    `
+  <div class="flex justify-between items-center">
+                    <div class="flex">
+                      <img src="${el.avatar}" alt="ava" />
+                      <div class="ml-4">
+                        <h1 class="font-semibold text-4 left-5">
+                        ${el.name}
+                        </h1>
+                        <h2
+                          class="font-normal text-[14px] left-[17px] text-[#555555] dark:text-white"
+                        >
+                        ${el.email}
+                        </h2>
+                      </div>
+                    </div>
+                    <img src="./img/gphoto.svg" alt="google" class="" />
+                  </div>
+                  <p
+                    class="w-[372px] h-[160px] font-normal text-[14px] left-[20px] mt-[14px]"
+                  >
+                  ${el.comment}
+                  </p>
+                  <div class="mt-[25px] h-[1.5px] bg-[#E5E7EA]"></div>
+                  <div class="flex items-center mt-[14px]">
+                    <h4
+                      class="font-medium text-[14px] inline-block left-[17px]"
+                    >
+                      Star rating:
+                    </h4>
+                    <img
+                      src="${el.rating}"
+                      alt="stars"
+                      class="ml-[17px]"
+                    />
+                  </div>
+  `
+  );
+  comment_list.append(element);
+  const scrbr = createElement(
+    "div",
+    `scrbr-item w-[${546 / comments.length}px] h-1 bg-white opacity-[0.4]`,
+    ""
+  );
+  scrollbar.append(scrbr);
+});
+document.querySelector(".scrbr-item").classList.add("active-scroll");
+
+count = 0;
+const slider__right = document.querySelector(".slider__right");
+const slider__left = document.querySelector(".slider__left");
+
+slider__right.addEventListener("click", (e) => {
+  document.querySelector(".active-scroll").classList.remove("active-scroll");
+  if (count === comments.length - 1) {
+    count = -1;
+  }
+  comment_list.style.transform = `translateX(-${++count * 420}px)`;
+  const scr = document.querySelectorAll(".scrbr-item");
+  scr[count].classList.add("active-scroll");
+});
+slider__left.addEventListener("click", (e) => {
+  document.querySelector(".active-scroll").classList.remove("active-scroll");
+
+  if (count === 0) {
+    count = comments.length;
+  }
+  comment_list.style.transform = `translateX(-${--count * 420}px)`;
+  const scr = document.querySelectorAll(".scrbr-item");
+  scr[count].classList.add("active-scroll");
+});
